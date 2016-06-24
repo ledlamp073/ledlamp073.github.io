919a6654cea442db5ad44735586805a5cd5704ae
????????????????????????????????????????????????????????»??????????????????????????????????????????????????????·????????????????????????????????????????????????????????°??????????????????????????????????????????????????????µ????????????????????????????????????????????????????»?? <??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????>
2016-06-25 02:25:36 +0500

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+(function ($) {
+    $.fn.serializeFormJSON = function () {
+
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+    
+    new WOW().init();
+    
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+    
+    $('#callback-form #submit-button').on('click', function(e) {
+      $.ajax({
+        url: $('#callback-form').attr('action'), 
+        method: "POST",
+        data: $('#callback-form').serializeFormJSON(),
+        dataType: "json"
+      });
+    });
+
+})(jQuery);
